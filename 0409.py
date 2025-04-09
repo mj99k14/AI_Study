5a70d47587f72138f8948538fc57b1f3ed6862b9
mj99k14 <ekfkawnl644@gmail.com>
2025-04-09 22:43:00 +0900

    수업업

--- 0409.py
+++ 0409.py
@@ -4,10 +4,13 @@ num_of_samples = 5
 num_of_features = 2
 
 np.random.seed(1)
+np.set_printoptions(False,suppress=True)
 x = np.random.rand(num_of_samples, num_of_features)  * 10
 x_ture = [5, 3]
 b_ture = 4
 
 print(x)
-print(x[0, 1])
-print(x[2, 0])
+print("----------------------")
+print(x[:, 0])
+print("----------------------")
+print(x[:, 1])
